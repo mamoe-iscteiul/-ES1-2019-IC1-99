5dce72ea40586b084d049588503a07860a80b24f
bjrcm <bjrcm@iscte-iul.pt>
2019-12-08 16:20:05 +0000

    metodos criados

---
+++ ProjetoES/src/Projeto/Metodos.java
@@ -0,0 +1,14 @@
+package Projeto;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Metodos {
+	public file openRead()throws IOException{
+		//File f= 
+	}
+	public static void main(String[] args) {
+		
+	}
+}
