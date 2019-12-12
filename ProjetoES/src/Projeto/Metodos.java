03f886ffc835accc65d93d7cf12e6be13b160f34
Marcelo Monteiro <mamoe@iscte-iul.pt>
2019-12-12 13:06:07 +0000

    tentativa de apagar tudo

---
+++ ProjetoES/src/Projeto/Metodos.java
@@ -0,0 +1,17 @@
+package Projeto;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Metodos {
+	public File openRead()throws IOException{
+		return null;//adicionei isto
+		//File f= 
+		//teste 123
+		//teste 456 ou 321
+	}
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ ProjetoES/src/module-info.java
@@ -0,0 +1,6 @@
+module projetoES {
+	requires javafx.graphics;
+	requires javafx.controls;
+	requires javafx.fxml;
+	
+}
